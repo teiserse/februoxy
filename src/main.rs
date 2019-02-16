206038838fc66ef7d3179bff1d31e4f17b0eca62
teisere <teiserse@tcd.ie>
2019-02-16 16:21:05 +0000

    partial(?) cache implementaion, does not compile in the slightest

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use hyper::{Client, Server};
+use hyper::{Client, Server, Response};
 use hyper::service::service_fn;
 use hyper::rt::{self, Future};
-use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::Arc;
+
 fn main() {
     let in_addr = ([127, 0, 0, 1], 3001).into();
-
     let client_main = Client::new();
+    let mut cache = Arc::new(HashMap::new());
 
     let new_service = move || {
         let client = client_main.clone();
+        let mut cache_access = Arc::clone(&cache);
         service_fn(move |req| {
             println!("{:?}", req);
-            client.request(req)
+            match cache_access.get(req.uri()) {
+                Some(res) => res,
+                None =>
+                    client.request(req).map(|res| {
+                        println!("{:?}", res.headers());
+                        cache_access.insert(req.uri(), res);
+                        res
+                    })
+            }
         })
     };
 
