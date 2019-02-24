c5424d78165ebc47922f219ad316b4b339cbdb5c
teisere <teiserse@tcd.ie>
2019-02-24 14:07:23 +0000

    Lifetimes are wrong somehow

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,54 @@ use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::rt::{self, Future};
 use futures::future::{self, Either};
-//use std::net::ToSocketAddrs;
 use tokio::prelude::*;
-use tokio::io::{copy, shutdown};
+use tokio::io::copy;
 use tokio::net::TcpStream;
-//use std::collections::HashMap;
-//use std::sync::{Arc, Mutex};
-
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+use std::collections::hash_map::RandomState;
 
 fn main() {
     let in_addr = ([127, 0, 0, 1], 3001).into();
     let client_main = Client::new();
-
-    //let mut cache = Arc::new(Mutex::new(HashMap::new()));
+    let blocked_domains = Arc::new(Mutex::new(vec!["www.example.com", "garfeet.me"]));
+    let response_cache: Arc<Mutex<HashMap<String, String, RandomState>>> = Arc::new(Mutex::new(HashMap::new()));
+    let current_conn = Arc::new(Mutex::new(0));
 
     let new_service = make_service_fn(move |conn: &AddrStream| {
         let _remote_addr = conn.remote_addr();
         let client = client_main.clone();
-        //let mut cache_access = Arc::clone(&cache);
+        let blocker = Arc::clone(&blocked_domains);
+        let cache = Arc::clone(&response_cache);
+        let id_maker = Arc::clone(&current_conn);
+        let conn_id: u64 = id_maker.lock().unwrap().clone();
+        *id_maker.lock().unwrap() += 1;
 
         service_fn(move |req: Request<Body>| {
-            println!("{:?}", req);
+            println!("Connection:  {}  -  Request for {}, Type: {}", conn_id, req.uri(), req.method());
             let destination = String::from(req.uri().authority_part().unwrap().as_str());
-            if destination.contains("example") {
+
+            let block_len = blocker.lock().unwrap().len().clone();
+            let mut is_blocked = false;
+
+            for iter in 0..block_len {
+                if destination.contains(blocker.lock().unwrap()[iter]) {
+                    is_blocked = true;
+                }
+            }
+
+            if is_blocked {
+                println!("Connection:  {}  -  Blocked Domain!", &conn_id);
                 Either::A(Either::A(
-                    future::ok(Response::new(hyper::Body::from("Another Example\n")))
+                    future::ok(Response::new(hyper::Body::from(
+                        "You have sent a request to a blocked domain.\
+                        \r\nThis infraction will not be tolerated.\
+                        \r\nYou will be reported to the system administrator.")))
                 ))
             } else if destination.contains(":443") {
-                println!("Attempted HTTPS Connection!");
-                //let dest_addr = destination.to_socket_addrs().unwrap().as_slice()[0];
-                /*
-                                let server = TcpStream::connect(&dest_addr).and_then(|server|{
-                                    let (client_reader, client_writer) = conn.split();
-                                    let (server_reader, server_writer) = server.split();
-
-                                    let client_to_server = copy(client_reader, server_writer);
-
-                                    let server_to_client = copy(server_reader, client_writer);
-
-                                    client_to_server.join(server_to_client)
-                                });*/
-
                 let into_tcp =
-                    req.into_body().on_upgrade().map_err(move |err| eprintln!("error: {}", err))
-                        .map(move |upgraded| {
-
+                    req.into_body().on_upgrade().map_err(move |err| eprintln!("Connection: {}  -  error: {}", &conn_id, err))
+                        .and_then(move |upgraded| {
                             let std_server = std::net::TcpStream::connect(&destination).unwrap();
 
                             let server = TcpStream::from_std(std_server, &tokio::reactor::Handle::default()).unwrap();
@@ -55,39 +58,37 @@ fn main() {
                             let (client_reader, client_writer) = upgraded.split();
                             let (server_reader, server_writer) = server.split();
                             let client_to_server = copy(client_reader, server_writer)
-                                .map(|(n, _, server_writer)| {
-                                println!("{} bytes!",n);
-                            });
+                                .map_err(move |err| eprintln!("Connection:  {}  -  error: {}", &conn_id, err))
+                                .map(move |(n, _, _)| {
+                                    if n != 0 { println!("Connection:  {}  -  {} bytes sent to server!", &conn_id, n) };
+                                });
 
                             let server_to_client = copy(server_reader, client_writer)
-                                .map(|(n, _, client_writer)| {
-                                println!("{} bytes!",n);
-                            });
+                                .map_err(move |err| eprintln!("Connection:  {}  -  error: {}", &conn_id, err))
+                                .map(move |(n, _, _)| {
+                                    if n != 0 { println!("Connection:  {}  -  {} bytes sent to client!", &conn_id, n) };
+                                });
 
                             client_to_server.join(server_to_client)
-
-                        }).map(move |_| {
-                        println!("testing!");
-                    });
+                        }).map(|_| { () });
                 tokio::spawn(into_tcp);
                 Either::A(Either::B(
                     future::ok(Response::new(hyper::Body::empty()))
                 ))
             } else {
                 Either::B(client.request(req).map(|res| {
-                    println!("{:?}", res.headers());
-                    //cache_access.lock().unwrap().insert("questionmark", "test");
+                    cache.lock().unwrap().insert(String::from("test"), String::from("ing"));
                     res
                 }))
             }
         })
     });
 
-    let server_old = Server::bind(&in_addr)
+    let server = Server::bind(&in_addr)
         .serve(new_service)
         .map_err(|e| eprintln!("server error: {}", e));
 
 
     println!("Listening on http://{}", in_addr);
-    rt::run(server_old);
+    rt::run(server);
 }
