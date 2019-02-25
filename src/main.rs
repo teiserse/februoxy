d2c0e54d03d7cf061369fe565716a2a7fadb6880
teisere <teiserse@tcd.ie>
2019-02-25 20:39:17 +0000

    Mostly working UI.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server};
 use hyper::{HeaderMap, StatusCode, Version};
 use std::collections::HashMap;
-use std::io::{stdout, stdin, Read, Write};
+use std::io::{stdout, Write};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use termion::{AsyncReader, async_stdin};
+use termion::async_stdin;
 use termion::raw::IntoRawMode;
-use termion::input::TermRead;
-use termion::event::Key;
 use tokio::io::copy;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
@@ -45,7 +43,7 @@ fn main() {
     let response_cache = Arc::new(Mutex::new(HashMap::new()));
     let current_conn = Arc::new(Mutex::new(0));
     let (send_out, send_in) = mpsc::channel();
-
+    
     let new_service = make_service_fn(move |conn: &AddrStream| {
         let _remote_addr = conn.remote_addr();
         let client = client_main.clone();
@@ -60,7 +58,7 @@ fn main() {
             output
                 .send((
                     conn_id,
-                    format!("Request for {}, Type: {}", req.uri(), req.method()),
+                    format!("{} request for {}", req.method(), req.uri()),
                 ))
                 .unwrap();
             let destination = String::from(req.uri().authority_part().unwrap().as_str());
@@ -161,15 +159,34 @@ fn main() {
                 match cache.lock().unwrap().get(&destination) {
                     None => {
                         let output = mpsc::Sender::clone(&output);
-                        output
-                            .send((
-                                conn_id.clone(),
-                                format!("URL not seen before, caching and returning."),
-                            ))
-                            .unwrap();
+                        let method_used = req.method().clone();
                         Either::B(Either::A(client.request(req).map({
                             let cache = Arc::clone(&cache);
                             move |res: Response<Body>| {
+                                if let Some(header) =
+                                    res.headers().get(hyper::header::CACHE_CONTROL)
+                                {
+                                    if method_used != hyper::Method::GET {
+                                        return res;
+                                    }
+                                    if header.to_str().unwrap().contains("no-cache") {
+                                        output
+                                            .send((
+                                                conn_id.clone(),
+                                                format!("URL not seen before, no-cache."),
+                                            ))
+                                            .unwrap();
+                                        return res;
+                                    }
+                                }
+
+                                output
+                                    .send((
+                                        conn_id.clone(),
+                                        format!("URL not seen before, caching and returning."),
+                                    ))
+                                    .unwrap();
+
                                 let (parts, body) = res.into_parts();
                                 let content = body
                                     .fold(Vec::new(), |mut acc, chunk| {
@@ -220,18 +237,32 @@ fn main() {
         })
     });
 
-    thread::spawn(move || {
+    let (send_end, get_end) = futures::sync::oneshot::channel::<()>();
+
+    let server = Server::bind(&in_addr)
+        .serve(new_service)
+        .with_graceful_shutdown(get_end)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    let mut stdin = async_stdin().bytes();
+
+//    let blocked_list = Arc::clone(&blocked_domains);
+
+    thread::spawn( move || {
         let mut stdout = stdout();
         if termion::is_tty(&stdout) {
             let mut stdout = stdout.lock().into_raw_mode().unwrap();
-            let mut stdin = stdin();
             let (width, height) = termion::terminal_size().unwrap();
-
+            let mut current_given = Vec::new();
+            let mut messages = Vec::new();
+            let mut prev_len = 0;
+            
             write!(
                 stdout,
-                "{}{}",
+                "{}{}{}",
                 termion::clear::All,
-                termion::cursor::Goto(1, 1)
+                termion::cursor::Goto(1, 1),
+                termion::cursor::Hide
             )
             .unwrap();
 
@@ -250,62 +281,189 @@ fn main() {
             .unwrap();
 
             for col in 2..width {
-                write!(stdout,"{}{}{}{}",
-                       termion::cursor::Goto(col,1),
-                       BOX_HORIZONTAL,
-                       termion::cursor::Goto(col,height),
-                       BOX_HORIZONTAL
-                    ).unwrap();
+                write!(
+                    stdout,
+                    "{}{}{}{}",
+                    termion::cursor::Goto(col, 1),
+                    BOX_HORIZONTAL,
+                    termion::cursor::Goto(col, height),
+                    BOX_HORIZONTAL
+                )
+                .unwrap();
             }
 
             for row in 2..height {
-                write!(stdout,"{}{}{}{}",
-                       termion::cursor::Goto(1,row),
-                       BOX_VERTICAL,
-                       termion::cursor::Goto(width,row),
-                       BOX_VERTICAL
-                       ).unwrap();
+                write!(
+                    stdout,
+                    "{}{}{}{}",
+                    termion::cursor::Goto(1, row),
+                    BOX_VERTICAL,
+                    termion::cursor::Goto(width, row),
+                    BOX_VERTICAL
+                )
+                .unwrap();
             }
 
             let boundary = (width / 3) * 2;
-            write!(stdout,"{}{}{}{}",
-                   termion::cursor::Goto(boundary,1),
-                   BOX_HORZ_DOWN,
-                   termion::cursor::Goto(boundary,height),
-                   BOX_HORZ_UP
-                   ).unwrap();
+            write!(
+                stdout,
+                "{}{}{}{}",
+                termion::cursor::Goto(boundary, 1),
+                BOX_HORZ_DOWN,
+                termion::cursor::Goto(boundary, height),
+                BOX_HORZ_UP
+            )
+            .unwrap();
 
             for row in 2..height {
-            write!(stdout,"{}{}",
-                   termion::cursor::Goto(boundary,row),
-                   BOX_VERTICAL
-                ).unwrap();
+                write!(
+                    stdout,
+                    "{}{}",
+                    termion::cursor::Goto(boundary, row),
+                    BOX_VERTICAL
+                )
+                .unwrap();
             }
 
+                write!(
+                    stdout,
+                    "{} C-ID {} Message{}{}{}{}{}FEBRUOXY - For CS3031",
+                    termion::cursor::Goto(2,2),
+                    BOX_VERTICAL,
+                    termion::cursor::Goto(8,1),
+                    BOX_HORZ_DOWN,
+                    termion::cursor::Goto(8,height),
+                    BOX_HORZ_UP,
+                    termion::cursor::Goto(boundary + 1,2)
+                ).unwrap();
+
+                write!(
+                    stdout,
+                    "{}{}{}{}{}{}{}{}",
+                    termion::cursor::Goto(1,3),
+                    BOX_VERT_RIGHT,
+                    BOX_HORIZONTAL.to_string().repeat(6),
+                    BOX_VERT_HORZ,
+                    BOX_HORIZONTAL.to_string().repeat((boundary - 9) as usize),
+                    BOX_VERT_HORZ,
+                    BOX_HORIZONTAL.to_string().repeat((width - boundary - 1) as usize),
+                    BOX_VERT_LEFT
+                ).unwrap();
+
+                write!(
+                    stdout,
+                    "{}Blocking List:",
+                    termion::cursor::Goto(boundary + 2, 4)
+                ).unwrap();
+/*
+                write!(
+                    stdout,
+                    "{}{:2}: {}{}{:2}:{}",
+                    termion::cursor::Goto(boundary + 1, 5),
+                    0,
+                    blocked_list.lock().unwrap()[0],
+                    termion::cursor::Goto(boundary + 1, 6),
+                    1,
+                    blocked_list.lock().unwrap()[1]
+                ).unwrap();
+*/
             stdout.flush().unwrap();
 
-            for input in stdin.keys() {
-                match input.unwrap() {
-                   Key::Char('q') => std::process::exit(0),
-                   Key::Char(given) => {write!(stdout,"{}{}",
-                       termion::cursor::Goto(50,50),
-                       given
-                       ).unwrap();
-                   stdout.flush().unwrap();
-                   },
-                   _ => {}
-            }}
+            loop {
+                let in_byte = stdin.next();
+                if in_byte.is_some() {
+                    match in_byte.unwrap() {
+                        Ok(27) => {
+                            writeln!(
+                                stdout,
+                                "{}{}",
+                                termion::cursor::Goto(1, height),
+                                termion::cursor::Show
+                            )
+                            .unwrap();
+                            send_end.send(()).unwrap();
+                            return;
+                        }
+                        Ok(127) => {
+                            current_given.pop();
+                        }
+                        Ok(num) => {
+                            current_given.push(num);
+                        }
+                        _ => {
+                            writeln!(
+                                stdout,
+                                "{}{}",
+                                termion::cursor::Goto(1, height),
+                                termion::cursor::Show
+                            )
+                            .unwrap();
+                            send_end.send(()).unwrap();
+                            return;
+                        }
+                    }
+                }
 
+                let get_message = send_in.try_recv();
+                match get_message {
+                    Ok(recieved) => {
+                        messages.push(recieved);
+                    }
+                    Err(_) => {}
+                }
+
+                let current_len = messages.len();
+                if { current_len != prev_len } {
+                    let mut line_number = 4;
+                    while line_number < height && (line_number as usize) < current_len {
+                        let (id, message) = if current_len < ((height+1) as usize) {
+                            &messages[(line_number - 4) as usize]
+                        } else {
+                            &messages[(current_len - 1) - (((height) - line_number - 1) as usize)]
+                        };
+                        let mess_to_print = if message.len() < ((boundary - 14) as usize) {
+                            message.clone()
+                        } else {
+                            let mut trimmed = message.clone();
+                            trimmed.truncate((boundary - 14) as usize);
+                            trimmed.push_str("...");
+                            trimmed
+                        };
+                        write!(
+                            stdout,
+                            "{}{}{}{:4}  {}  {}",
+                            termion::cursor::Goto(2, line_number),
+                            " ".repeat((boundary - 2) as usize),
+                            termion::cursor::Goto(2, line_number),
+                            id % 10000,
+                            BOX_VERTICAL,
+                            mess_to_print
+                        )
+                        .unwrap();
+                        line_number += 1;
+                    }
+                    prev_len = current_len;
+                }
+
+                write!(
+                    stdout,
+                    "{}{}{}{}",
+                    termion::cursor::Goto(boundary + 1, height - 1),
+                    " ".repeat((width - boundary - 1) as usize),
+                    termion::cursor::Goto(boundary + 1, height - 1),
+                    std::str::from_utf8(&current_given).unwrap()
+                )
+                .unwrap();
+
+                stdout.flush().unwrap();
+                thread::sleep(Duration::from_millis(20));
+            }
         } else {
             for message in send_in {
                 writeln!(stdout, "Connection:  {}  -  {}", message.0, message.1).unwrap();
             }
         }
     });
-
-    let server = Server::bind(&in_addr)
-        .serve(new_service)
-        .map_err(|e| eprintln!("server error: {}", e));
 
     rt::run(server);
 }
